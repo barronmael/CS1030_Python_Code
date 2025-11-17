12a08cb84ed3d134df3af3a351484f41b2a3893c
barronmael <barronmael31@gmail.com>
2025-11-17 12:28:32 -0700

    Trouble shooting BMI LAB
    +scratch work for tech doc
    +Password gen created

--- Practice_Code/scratch_work.py
+++ Practice_Code/scratch_work.py
@@ -3,3 +3,23 @@ if number > 0:
     print("Positive number")
 else:
     print("Non-positive number")
+
+name = "Ish"    # string
+PI = 3.14159        # float
+radius = 20         # integer
+is_student= True    # boolean
+
+PI = 3.14159
+MAX_USERS = 100
+
+age = int(input("Enter your age: "))
+print(f"Next year, you’ll be {age + 1}!")
+
+years = 15
+#PI = 3.14
+#shape = "Circle"
+#old = True
+
+name = input("Enter your name: ")
+print(f"Hello {name}!")
+
